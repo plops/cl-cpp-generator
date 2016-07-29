bc60040b1fcf10d750dfc73794e09e9b5869c188
Martin Kielhorn <martin.kielhorn@ezono.com>
2016-07-29 14:14:25 +0200

    i don't think i want to implement indenting

--- o.cpp
+++ o.cpp
@@ -1,37 +1,38 @@
-#include <stdio.h>
 #include "bla.h"
+#include <stdio.h>
 namespace N {
-class gug::senso {public:
+class gug::senso {
+public:
+  int f(int a);
+  int h(int a);
 
-int f(int a);
-int h(int a);
 private:
-
-int f2(int a);
-int h2(int a);
+  int f2(int a);
+  int h2(int a);
 
 }
 
-class sensor  : public p::pipeline, virtual public qqw::q, virtual public qq::q{
+class sensor : public p::pipeline,
+               virtual public qqw::q,
+               virtual public qq::q {
 }
 
-class lag::sensor2  : private p::pipeline2{
+class lag::sensor2 : private p::pipeline2 {
 }
 
 int i = 0;
-float f = (3.2000000000f-7);
+float f = (3.2000000000f - 7);
 double d = (7.200000000000000000e-31);
-complex float z = ((2.0000000000f+0) + (1.0000000000f+0i));
+complex float z = ((2.0000000000f + 0) + (1.0000000000f + 0i));
 complex double w = ((2.000000000000000000e+0) + (1.000000000000000000e+0i));
 
-complex double::blub g(char a,int* b){
-  3 = (+ 1 2 3);
-43 = (+ 1 2 3);
-;
-  3 += (* 2 3 4);
+complex double ::blub g(char a, int *b) {
+  {
+    3 = (+1 2 3);
+    43 = (+1 2 3);
+    ;
+    3 += (*2 3 4);
+  };
   ((3 / (32 + 3)) * 2 * 3 * (2 + (13 / (2 + 39))));
-
 }
-
- };
-
+};
