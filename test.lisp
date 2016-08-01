77c89238a4634fd462621c8524508c8ce18fef2b
Martin Kielhorn <martin.kielhorn@ezono.com>
2016-08-01 11:57:48 +0200

    more unit tests

--- test.lisp
+++ test.lisp
@@ -1,10 +1,11 @@
 ;; http://www.sbcl.org/manual/ 16.3 sb-cover
-(require :sb-cover)
+(eval-when (:compile-toplevel)
+  (require :sb-cover)
 
-(declaim (optimize sb-cover:store-coverage-data))
+  (declaim (optimize sb-cover:store-coverage-data))
 
-(compile-file "cp.lisp")
-(load "cp.fasl")
+  (compile-file "cp.lisp")
+  (load "cp.fasl"))
 
 (in-package :g)
 
@@ -20,26 +21,51 @@
       str)))
 
 
-(string=
- (clang-format 
-  (emit-cpp :str nil :code
-	    '(with-compilation-unit
-	      (for ((i a :type int) (< i n) (+= i 1))
-	       (+= b q)))))
- (clang-format
-  "for (int i = a; i < n; i += 1) {
+
+(defun test (code string)
+  (assert (string=
+	   (clang-format (emit-cpp :str nil :code code))
+	   (clang-format string))))
+
+(progn  ;; for loop
+  (test 
+   '(with-compilation-unit
+     (for ((i a :type int) (< i n) (+= i 1))
+      (+= b q)))
+   "for (int i = a; i < n; i += 1) {
+b += q;
+}
+")
+
+
+  (test 
+   '(with-compilation-unit
+     (for (() (< i n) (+= i 1))
+      (+= b q)))
+   "for (; i < n; i += 1) {
+b += q;
+}
+")
+
+  (test 
+   '(with-compilation-unit
+     (for ((i a :type int) () (+= i 1))
+      (+= b q)))
+   "for (int i = a; ; i += 1) {
+b += q;
+}
+")
+
+  (test 
+   '(with-compilation-unit
+     (for ((i a :type int) (< i n) ())
+      (+= b q)))
+   "for (int i = a; i < n;) {
 b += q;
 }
 "))
 
-(assert
- (string= 
-  (with-output-to-string (s)
-    (emit-cpp :str s :code
-	      '(with-compilation-unit
-		(for ((i a :type int) (< i n) (+= i 1))
-		 (+= b q))
-		)))
-  "for(int i = a; i < n; i += 1) { b += q; }"))
-
 (sb-cover:report "/home/martin/stage/cl-cpp-generator/cover/")
+
+#+nil
+(sb-cover:reset-coverage)
