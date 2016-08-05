3d35243ce3483f28ba2d30adb18fd9598007f6f3
Martin Kielhorn <martin.kielhorn@ezono.com>
2016-08-05 09:34:28 +0200

    evaluate sub expressions in list and comma-list

--- cp.lisp
+++ cp.lisp
@@ -175,9 +175,9 @@
 	 (array+lenbytes (destructuring-bind (name) (cdr code)
 		       (format str "~a, sizeof( ~a )" name name)))
 	 (list (destructuring-bind (&rest rest) (cdr code)
-		 (format str "{~{~a~^,~}}" rest)))
+		 (format str "{~{~a~^,~}}" (mapcar #'(lambda (x) (emit-cpp :code x)) rest))))
 	 (comma-list (destructuring-bind (&rest rest) (cdr code)
-		  (format str "~{~a~^,~}" rest)))
+		  (format str "~{~a~^,~}" (mapcar #'(lambda (x) (emit-cpp :code x)) rest))))
 	 (lisp (eval (cadr code)))
 	 (statement ;; add semicolon
 	  (cond ((member (second code) (append *binary-operator-symbol*
